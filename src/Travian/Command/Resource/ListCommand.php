8f10734d2f0535833f507737d8346ffec9d4d673
Wysaard <mail@wysaard.nl>
2016-08-20 23:23:20 +0200

    Make the displaying of upgrades optional

--- src/Travian/Command/Resource/ListCommand.php
+++ src/Travian/Command/Resource/ListCommand.php
@@ -4,6 +4,7 @@ namespace Timpack\Travian\Command\Resource;
 
 
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Timpack\Travian\Command\AbstractCommand;
 use Timpack\Travian\Model\Resource\Field;
@@ -14,7 +15,8 @@ class ListCommand extends AbstractCommand
     protected function configure()
     {
         $this->setName('resource:list')
-            ->setDescription('Get a list of all resources');
+            ->setDescription('Get a list of all resources')
+            ->addOption('show-upgrades', NULL, InputOption::VALUE_NONE, 'Show the costs of upgrading the structure as well');
 
         parent::configure();
     }
@@ -25,19 +27,30 @@ class ListCommand extends AbstractCommand
         $resources = new Resource();
         $rows = [];
 
-        /** @var Field $resourceField */
-        foreach ($resources->getResourceList() as $resourceField) {
-            $rows[] = [
-                'id' => $resourceField->constructionId,
-                'type' => $resourceField->type,
-                'level' => $resourceField->level,
-                'clay' => $resourceField->upgradeInfo->clay,
-                'lumber' => $resourceField->upgradeInfo->lumber,
-                'iron' => $resourceField->upgradeInfo->iron,
-                'crop' => $resourceField->upgradeInfo->crop
-            ];
+        if ($input->getOption('show-upgrades')){
+            foreach ($resources->getResourceList() as $resourceField) {
+                
+                $rows[] = [
+                    'id' => $resourceField->constructionId,
+                    'type' => $resourceField->type,
+                    'level' => $resourceField->level,
+                    'clay' => $resourceField->upgradeInfo->clay,
+                    'lumber' => $resourceField->upgradeInfo->lumber,
+                    'iron' => $resourceField->upgradeInfo->iron,
+                    'crop' => $resourceField->upgradeInfo->crop
+                ];
+            }
+        } else {
+            foreach ($resources->getResourceList() as $resourceField) {
+                
+                $rows[] = [
+                    'id' => $resourceField->constructionId,
+                    'type' => $resourceField->type,
+                    'level' => $resourceField->level
+                ];
+            }
         }
-
+        
         $renderer->render($output, $rows);
     }
 
